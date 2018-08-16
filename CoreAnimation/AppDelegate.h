0f6284c78c0627e3f6689754350f095467fd705e
JnKindle <Jn_never2013@163.com>
2018-08-16 09:28:16 +0800

    Initial Commit

---
+++ CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by Jn_Kindle on 2018/8/16.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
